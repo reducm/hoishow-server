9bac642a771721d9a0cbbc624434f9f13071419d
xofred <fwin3000@gmail.com>
2015-03-30 14:30:38 +0800

    show的index

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -26,4 +26,20 @@ describe Show do
       expect(show.stars).to eq show.concert.stars
     end
   end
+
+  context "test paginate" do
+    before('each') do
+      200.times do
+        create :show
+      end
+    end
+    it "per 20" do
+      expect( Show.page(2).size ).to eq 20
+    end
+
+    it "page2's model should be correct" do
+      shows = Show.all
+      expect(shows.index Show.page(2).first).to be 20
+    end
+  end
 end
