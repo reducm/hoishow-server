8fe74bdb9c291ccc6aebf604dcc2a169f1db4cc7
tom <tom@bestapp.us>
2015-03-31 15:24:28 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+city = JSON.parse(File.open("/Users/tangwenjie/projects/hoishow-server/db/city_district.json","r").read)
+
+num = 0
+city_count = city.count
+
+
+while (num < city_count) do
+  City.where(name: city[num]["name"]).first_or_initialize.update_attributes!(pinyin: city[num]["pinyin"], code: city[num]["code"], is_hot: city[num]["hot"] || false)
+
+  district_array = city[num]["districts"]
+  district_array_count = district_array.count
+  i = 0
+  while(i < district_array_count) do
+    District.where(name: district_array[i]["name"]).first_or_initialize.update_attributes!(city_id: district_array[i]["city_id"])
+    i += 1
+  end
+
+  num += 1
+
+end
