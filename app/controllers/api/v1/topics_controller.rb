57b4b25bfb46cf5d04eb970bcfa487e170021379
xofred <fwin3000@gmail.com>
2015-04-14 13:02:31 +0800

    topic index

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::TopicsController < ApplicationController
   def index
     params[:page] ||= 1
-    if params[:subject_type].nil? || params[:subject_id].nil?
-      @topics = Topic.page(params[:page])
-    else
+    if params[:subject_type] && params[:subject_id]
       @topics = Topic.where("subject_type = ? AND subject_id = ?", params[:subject_type], params[:subject_id]).page(params[:page])
+    else
+      @topics = Topic.page(params[:page])
     end
   end
 
   def show
-   @topic = Topic.find(params[:id]) 
+    @topic = Topic.find(params[:id]) 
   end
 end
