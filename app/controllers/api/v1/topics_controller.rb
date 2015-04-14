e40493c97c77ee9100945c9b218b9cf644c721e1
xofred <fwin3000@gmail.com>
2015-04-14 16:54:20 +0800

    topic index传参判断

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TopicsController < ApplicationController
   def index
     params[:page] ||= 1
-    if params[:subject_type] && params[:subject_id]
+    if params[:subject_type].present? && params[:subject_id].present? 
       @topics = Topic.where("subject_type = ? AND subject_id = ?", params[:subject_type], params[:subject_id]).page(params[:page])
     else
       @topics = Topic.page(params[:page])
