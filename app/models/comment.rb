338e93e6cdc49776fd5bb53ef9b5a55e9a5ceb5a
karra <karra@bestapp.us>
2015-04-15 14:12:22 +0800

    add exceptionnotifier to rescue

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,8 +11,9 @@ class Comment < ActiveRecord::Base
 
   def subject
     begin
-      Object::const_get(subject_type).where(id: subject_id).first 
-    rescue 
+      Object::const_get(subject_type).where(id: subject_id).first
+    rescue => e
+      ExceptionNotifier::Notifier.background_exception_notification(e).deliver_now
       Rails.logger.fatal("subject wrong, comment_id: #{ id }, subject_type: #{subject_type}, subject_id: #{subject_id}")
       nil
     end
