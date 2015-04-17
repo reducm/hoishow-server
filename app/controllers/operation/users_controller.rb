20ca16caff1c7166a9ae67919c9501432fcc559a
karra <karra@bestapp.us>
2015-04-17 17:50:29 +0800

    add operation users list

--- app/controllers/operation/users_controller.rb
+++ app/controllers/operation/users_controller.rb
@@ -1,4 +1,12 @@
 class Operation::UsersController < Operation::ApplicationController
   before_filter :check_login!
   load_and_authorize_resource
+
+  def index
+    if params[:q]
+      @users = User.search(params[:q])
+    else
+      @users = User.page(params[:page])
+    end
+  end
 end
