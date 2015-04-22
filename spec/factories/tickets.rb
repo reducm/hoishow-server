c59833f55be2db83975889ca60451265af51978b
tom <tom@bestapp.us>
2015-04-22 10:25:47 +0800

    文档的star对象添加粉丝数量，完善了show的index页面

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -4,12 +4,19 @@ FactoryGirl.define do
   factory :ticket do
     area_id 1 
     show_id 1
-    price "9.99"
+    price 9.99
     order_id 1
     code "MyString"
-    code_valid_time "2015-04-08 16:49:44"
+    code_valid_time Time.now
     association :area
     association :show
     association :order
+    factory :success_ticket do
+      status Ticket.statuses[:success]
+    end
+    factory :used_ticket do
+      status Ticket.statuses[:used]
+    end
+
   end
 end
