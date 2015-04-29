d0d078b4ada69c9e20148ec616115061011946cc
karra <karra@bestapp.us>
2015-04-29 14:47:20 +0800

    fix admin set password

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -21,10 +21,29 @@ class Admin < ActiveRecord::Base
   end
 
   def set_password(password)
-    self.encrypted_password = PasswordHash.createHash(password)
+    self.createHash(password)
   end
 
   def password_valid?(password)
-    PasswordHash.validatePassword(password, self.encrypted_password)
+    self.validatePassword(password)
+  end
+
+  # Returns a salted PBKDF2 hash of the password.
+  def createHash(password)
+    self.salt = SecureRandom.base64(24)
+    pbkdf2 = OpenSSL::PKCS5::pbkdf2_hmac_sha1(password, self.salt, 1000, 24)
+    self.encrypted_password = ["sha1", Base64.encode64(pbkdf2)].join(':')
+  end
+
+  # Checks if a password is correct given a hash of the correct one.
+  # correctHash must be a hash string generated with createHash.
+  def validatePassword(password)
+    params = self.encrypted_password.split(':')
+    return false if params.length != 2
+
+    pbkdf2 = Base64.decode64(params[1])
+    testHash = OpenSSL::PKCS5::pbkdf2_hmac_sha1(password, self.salt, 1000, pbkdf2.length)
+
+    return pbkdf2 == testHash
   end
 end
