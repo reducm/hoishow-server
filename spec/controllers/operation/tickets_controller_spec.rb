abbff08527199018b2fc60212e0473ca8bd3987e
tom <tom@bestapp.us>
2015-05-08 14:59:13 +0800

    merge

--- spec/controllers/operation/tickets_controller_spec.rb
+++ spec/controllers/operation/tickets_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Operation::TicketsController, :type => :controller do
+  render_views
 
+  context "#index" do
+    before('each') do
+      admin = create :admin
+      session[:admin_id] = admin.id
+      30.times do |i|
+        create(:ticket, code: i)
+      end
+    end
+
+    it "should get 20 (base on model paginates_per) shows" do
+      get :index
+      binding.pry
+    end
+
+  end
 end
