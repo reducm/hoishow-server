d6257f28b91cd300d8b8b7155743c0a814439e63
tom <tom@bestapp.us>
2015-05-11 14:26:20 +0800

    added concert action:new

--- spec/controllers/operation/concerts_controller_spec.rb
+++ spec/controllers/operation/concerts_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Operation::ConcertsController, :type => :controller do
+  render_views
+  before('each') do
+
+
+  end
+
+  context "" do
+    it "" do
+
+    end
+
+  end
 
 end
