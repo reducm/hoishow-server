b2444659d5fa61d9bea7db0a104a135cd5fdacb7
karra <karra@bestapp.us>
2015-05-14 18:03:30 +0800

    fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 #encoding: UTF-8
 class Order < ActiveRecord::Base
-  ORDER_STATUS_PENDING = "PENDING" #待支付
-  ORDER_STATUS_PAID = "PAID" #已支付
-  ORDER_STATUS_SUCCESS = "SUCCESS" #成功出票
-  ORDER_STATUS_REFUND = "REFUND" #退款
-  ORDER_STATUS_OUTDATE = "OUTDATE"
+  ORDER_STATUS_PENDING = 0 #待支付
+  ORDER_STATUS_PAID = 1 #已支付
+  ORDER_STATUS_SUCCESS = 2 #成功出票
+  ORDER_STATUS_REFUND = 3 #退款
+  ORDER_STATUS_OUTDATE = 4
 
   belongs_to :user
   #Order创建的时候，要保存concert, stadium,city,show的name和id，用冗余避免多表查询
