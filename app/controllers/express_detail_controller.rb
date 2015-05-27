bed575d7cef34ee339af5217de0c78ea16c98550
karra <karra@bestapp.us>
2015-05-27 14:08:02 +0800

    add query express info

---
+++ app/controllers/express_detail_controller.rb
@@ -0,0 +1,12 @@
+include QueryExpress
+
+class ExpressDetailController < ApplicationController
+  layout false
+
+  def show
+    data = get_express_info(params[:code])["result"]
+    if data.any?
+      @result = data
+    end
+  end
+end
---
+++ app/helpers/express_detail_helper.rb
@@ -0,0 +1,2 @@
+module ExpressDetailHelper
+end
