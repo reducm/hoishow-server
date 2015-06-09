3863f4066e2155d4a596ec053f99567aed70a7e3
xofred <fwin3000@gmail.com>
2015-06-09 14:49:21 +0800

    订单列表筛选、导出

--- app/assets/javascripts/operation/orders.js.coffee
+++ app/assets/javascripts/operation/orders.js.coffee
@@ -1,12 +1,4 @@
 $ ->
-  $('#order_status_select').change ->
-    $(".order_status_cn").parent().show()
-    vtxt = $('#order_status_select').val()
-    if vtxt == "全部"
-      location.reload()
-    else
-      $(".order_status_cn:not(:contains('" + vtxt + "'))").parent().hide()
-
 #修改快递单号
   $("#expresses_list").on "click", ".change_express_data", (e) ->
     e.preventDefault()
--- app/controllers/operation/orders_controller.rb
+++ app/controllers/operation/orders_controller.rb
@@ -5,34 +5,49 @@ class Operation::OrdersController < Operation::ApplicationController
 
   def index
     params[:page] ||= 1
-    @orders = Order.page(params[:page]).order("created_at desc")
-    @r_ticket_orders = Kaminari.paginate_array(Order.orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
-    respond_to do |format|
-      format.html
-      format.csv { send_data @orders.to_csv(encoding: "UTF-8") }
-      format.xls
+    #搜索
+    if params[:q]
+      ids = User.where("nickname like ? or mobile like ?", "%#{params[:q]}%", "%#{params[:q]}%").map(&:id).compact
+      @orders = Order.where("show_name like ? or user_id in (?)", "%#{params[:q]}%", ids).page(params[:page]).order("created_at desc")
+      @r_ticket_orders = Kaminari.paginate_array(Order.orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
+    elsif params[:q_express_express]
+      @orders = Order.page(params[:page]).order("created_at desc")
+      @r_ticket_orders = Kaminari.paginate_array(Order.where("user_name like ? or user_mobile like ? or user_address like ?", "%#{params[:q_express]}%","%#{params[:q_express]}%", "%#{params[:q_express]}%").orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
+    else
+      @orders = Order.page(params[:page]).order("created_at desc")
+      @r_ticket_orders = Kaminari.paginate_array(Order.orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
     end
-  end
-
-  def search
-    params[:page] ||= 1
-    ids = User.where("nickname like ? or mobile like ?", "%#{params[:q]}%", "%#{params[:q]}%").map(&:id).compact
-    @orders = Order.where("show_name like ? or user_id in (?)", "%#{params[:q]}%", ids).page(params[:page]).order("created_at desc")
-    @r_ticket_orders = Kaminari.paginate_array(Order.orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
-    render :index
-  end
-
-  def search_express
-    params[:page] ||= 1
-    @orders = Order.page(params[:page]).order("created_at desc")
-    @r_ticket_orders = Kaminari.paginate_array(Order.where("user_name like ? or user_mobile like ? or user_address like ?", "%#{params[:q]}%","%#{params[:q]}%", "%#{params[:q]}%").orders_with_r_tickets.select { |order| order.show.r_ticket? }).page(params[:page]).per(10)
-    render :index
+    #下拉框筛选
+    case params[:order_status_select]
+    when "pending"
+      @orders = @orders.where(status: 0)
+      @r_ticket_orders = @r_ticket_orders.where(status: 0)
+    when "paid"
+      @orders = @orders.where(status: 1)
+      @r_ticket_orders = @r_ticket_orders.where(status: 1)
+    when "success"
+      @orders = @orders.where(status: 2)
+      @r_ticket_orders = @r_ticket_orders.where(status: 2)
+    when "refund"
+      @orders = @orders.where(status: 3)
+      @r_ticket_orders = @r_ticket_orders.where(status: 3)
+    when "outdate"
+      @orders = @orders.where(status: 4)
+      @r_ticket_orders = @r_ticket_orders.where(status: 4)
+    end
+    #导出
+    filename = Time.now.strfcn_time + '订单列表'
+    respond_to do |format|
+      format.html { render :index }
+      format.csv { send_data @orders.to_csv, filename: filename + '.csv'}
+      format.xls { headers["Content-Disposition"] = "attachment; filename=\"#{filename}.xls\""}
+    end
   end
 
   def show
     @order = Order.find(params[:id])
   end
-  
+
   def update_express_id
     @order = Order.find(params[:order_id])
     if @order.update!(express_id: params[:content], status: 2)
