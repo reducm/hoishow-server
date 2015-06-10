118c36b904e0be7411c9d3277fbe80b6d62108ff
tom <tom@bestapp.us>
2015-06-10 17:37:04 +0800

    message button

--- app/controllers/operation/topics_controller.rb
+++ app/controllers/operation/topics_controller.rb
@@ -38,7 +38,6 @@ class Operation::TopicsController < Operation::ApplicationController
       flash[:notice] = '内容修改成功'
     end
 
-    flash[:notice] = "互动修改成功"
     redirect_to edit_operation_topic_url(@topic)
   end
 
