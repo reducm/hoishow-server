122c2fd7c410ebbb67f8842cf340a89407397a82
karra <karra@bestapp.us>
2015-06-10 17:13:24 +0800

    fix

--- app/controllers/seats_map_controller.rb
+++ app/controllers/seats_map_controller.rb
@@ -4,6 +4,5 @@ class SeatsMapController < ApplicationController
     @show = Show.find_by_id(params[:show_id])
     @area = @show.areas.find_by_id(params[:area_id])
     @seats = @show.seats.where(area_id: @area.id)
-    @check_seats = @seats.where('id in (?)', params[:seats])
   end
 end
