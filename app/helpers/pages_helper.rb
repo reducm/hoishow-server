2cd2d47cfc9a4704aaa88d937ed885c82c406606
tom <tom@bestapp.us>
2015-06-11 20:11:53 +0800

    show share

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def get_ticket_price(show)
+    if show.selectable?
+      show.seats.map{|relation| relation.price.to_i}.uniq.join("/")
+    elsif show.selected?
+      show.show_area_relations.map{|relation| relation.price.to_i}.uniq.join("/")
+    end
+  end
 end
