e618ddac4b446bfa814223c70a45b39e9204af00
tom <tom@bestapp.us>
2015-06-14 12:38:54 +0800

    fix sharing_show style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,10 @@ class PagesController < ApplicationController
   def about
   end
 
+  def download
+    render layout: 'mobile'
+  end
+
   def wap_index
   end
 
@@ -15,25 +19,21 @@ class PagesController < ApplicationController
 
   def sharing_show
     @show = Show.find_by_id(params[:show_id])
-
     render layout: 'mobile'
   end
 
   def sharing_concert
     @concert = Concert.find_by_id(params[:concert_id])
-    cities = @concert.cities.limit(10)
+    cities = @concert.cities
     result ||= []
     cities.each do |city|
       hash = city.attributes
       hash.merge!({"vote_count" => UserVoteConcert.where(concert_id: @concert.id, city_id: city.id).count + @concert.concert_city_relations.find_by_city_id(city.id).try(:base_number).to_i})
       result.push(hash)
     end
-
     #sort
     result = result.sort!{|x, y| x["vote_count"] <=> y["vote_count"]}.reverse
-    
-    @cities = result
-
+    @cities = result[0..9]
     render layout: 'mobile'
   end
 end
