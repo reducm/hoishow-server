43f39ae175af693bb3d75ba0f6f2873b0acef02e
karra <karra@bestapp.us>
2015-06-18 17:41:54 +0800

    fix

--- app/controllers/operation/sessions_controller.rb
+++ app/controllers/operation/sessions_controller.rb
@@ -7,14 +7,16 @@ class Operation::SessionsController < Operation::ApplicationController
 
   def create
     admin = Admin.find_by_name(params[:session][:username])
-    if verify_block?(admin)
-      flash[:alert] = "账户被锁定，请联系管理员"
-      redirect_to operation_signin_url
-    elsif admin && admin.password_valid?(params[:session][:password])
-      admin.update(last_sign_in_at: DateTime.now)
-      session[:admin_id] = admin.id
+    if admin
+      if verify_block?(admin)
+        flash[:alert] = "账户被锁定，请联系管理员"
+        redirect_to operation_signin_url
+      elsif admin.password_valid?(params[:session][:password])
+        admin.update(last_sign_in_at: DateTime.now)
+        session[:admin_id] = admin.id
 
-      redirect_to operation_root_url
+        redirect_to operation_root_url
+      end
     else
       flash[:alert] = "账号或密码错误"
       redirect_to operation_signin_url
