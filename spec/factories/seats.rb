c0ccb226230701a13490fd87738f987d89b9d06f
Jed_Huo <jed@bestapp.us>
2015-07-03 16:41:41 +0800

    add seat_selection_logic spec test

--- spec/factories/seats.rb
+++ spec/factories/seats.rb
@@ -2,5 +2,20 @@
 
 FactoryGirl.define do
   factory :seat do
+    name 'x排x座'
+
+    trait :avaliable do
+      status 0
+    end
+
+    trait :locked do
+      status 1
+    end
+
+    trait :unused do
+      status 2
+    end
+
+    factory :avaliable_seat, traits: [:avaliable]
   end
 end
