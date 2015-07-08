4b2835fab529674821922b0f14dbb7fc9cc0decb
Jed_Huo <jed@bestapp.us>
2015-07-08 22:27:26 +0800

    move the api_verify method

--- app/controllers/open/v1/application_controller.rb
+++ app/controllers/open/v1/application_controller.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class Open::V1::ApplicationController < ApplicationController
+  before_filter :api_verify
 
   protected
   def show_auth!
@@ -21,6 +22,45 @@ class Open::V1::ApplicationController < ApplicationController
     @error_code = 4004
     @message = msg
     respond_to { |f| f.json }
+  end
+
+  #文档中的必需参数
+  def auth_params
+    params.permit(
+      "api_key",
+      "timestamp",
+      "show_id",
+      "area_id",
+      "user_id",
+      "mobile",
+      "quantity",
+      "reason"
+     )
+  end
+
+  def api_verify
+    return true if Rails.env.development?
+
+    if params[:api_key].blank? || params[:sign].blank? || params[:timestamp].blank?
+      return render json: {result_code: "1003", message: "缺少必要的参数"}
+    end
+
+    @auth = Rails.cache.fetch("api_auth_#{params[:api_key]}") do
+      ApiAuth.where(key: params[:api_key]).first
+    end
+
+    unless @auth
+      return render json: {result_code: "1001", message: "商户信息不存在"}
+    end
+
+    #签名中的时间戳，有效时间为10分钟
+    if Time.now - Time.at(params[:timestamp].to_i) > 600
+      return render json: {result_code: "1002", message: "签名验证不通过"}
+    end
+
+    unless params[:sign] == @auth.generated_sign(auth_params)
+      return render json: {result_code: "1002", message: "签名验证不通过"}
+    end
   end
 
 end
