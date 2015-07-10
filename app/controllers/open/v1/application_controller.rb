e74168080a28744c66688392e1c8ca9f4274717d
Jed_Huo <jed@bestapp.us>
2015-07-10 16:23:24 +0800

    set a open api logger

--- app/controllers/open/v1/application_controller.rb
+++ app/controllers/open/v1/application_controller.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 class Open::V1::ApplicationController < ApplicationController
-  before_filter :api_verify
+  before_filter :set_logger!, :api_verify
   skip_before_filter :verify_authenticity_token
+  attr_reader :logger
+  rescue_from StandardError, with: :log_error
 
   protected
   def show_auth!
@@ -60,6 +62,17 @@ class Open::V1::ApplicationController < ApplicationController
     unless params[:sign] == @auth.generated_sign(auth_params)
       return render json: {result_code: "1002", message: "签名验证不通过"}
     end
+  end
+
+  def set_logger!
+    @logger ||= Logger.new(File.join(Rails.root, 'log', 'open_api.log'), 'weekly')
+    @logger.info("\nStarted #{request.request_method} #{request.fullpath} for " \
+      "#{request.env['HTTP_HOST']} at #{Time.now}\n Params: #{params.to_hash}\n\n")
+  end
+
+  def log_error(e)
+    logger.error(e)
+    error_respond(5000, '服务器挂了')
   end
 
 end
