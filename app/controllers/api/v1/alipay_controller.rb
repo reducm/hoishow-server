cccbc569ea628d4f2a5a1483670b510bb18b96e7
karra <karra@bestapp.us>
2015-07-16 11:47:20 +0800

    fix bug

--- app/controllers/api/v1/alipay_controller.rb
+++ app/controllers/api/v1/alipay_controller.rb
@@ -39,7 +39,6 @@ class Api::V1::AlipayController < Api::V1::ApplicationController
     alipay_params = params.except(*request.path_parameters.keys)
     wp_print("alipay_params: #{alipay_params}")
     @order = Order.where(out_id: params[:out_trade_no]).first
-    @payment = @order.alipay_pay
     #订单已经是支付成功或已经进入票务流程或失败或退款时，不需要再改变订单状态
     return true if @order.success? || @order.refund? || @order.outdate?
     if Alipay::Notify.app_verity?(alipay_params) && @order.amount.to_f == alipay_params[:total_fee].to_f
