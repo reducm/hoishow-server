c7686f60a541b85ec307a690994746a053498192
Jed_Huo <jed@bestapp.us>
2015-07-21 17:47:40 +0800

    fix schema bug

--- db/schema.rb
+++ db/schema.rb
@@ -194,7 +194,7 @@ ActiveRecord::Schema.define(version: 20150721062235) do
     t.boolean  "is_sold_out", limit: 1,                           default: false
     t.integer  "seats_count", limit: 4
     t.string   "channels",    limit: 255
-    t.integer  "left_seats",  limit: 4
+    t.integer  "left_seats",  limit: 4,                           default: 0
   end
 
   add_index "show_area_relations", ["show_id", "area_id"], name: "index_show_area_relations_on_show_id_and_area_id", using: :btree
