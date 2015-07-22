777992d6bbdf960087032c7738e948f5ff73b1ac
karra <karra@bestapp.us>
2015-07-22 11:44:43 +0800

    add cache to model and add session expired time

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,5 +1,7 @@
 #encoding: UTF-8
 class Star < ActiveRecord::Base
+  acts_as_cached(:version => 1, :expires_in => 1.week)
+
   include ModelAttrI18n
   default_scope {order(:position)}
   has_many :videos, dependent: :destroy
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hoishow-server_session'
+Rails.application.config.session_store(
+ :cookie_store,
+ key: '_hoishow-server_session',
+ expire_after: 1.day
+)
