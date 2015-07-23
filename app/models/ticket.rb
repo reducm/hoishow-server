54ed5a3cbe6b359c48a78736cab5776a7adcc98a
tom <tom@bestapp.us>
2015-07-23 16:08:04 +0800

    fix tiket order

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 #encoding: UTF-8
 class Ticket < ActiveRecord::Base
   serialize :channels
-  #default_scope {order('created_at DESC')}
+  default_scope {order('created_at DESC')}
 
   belongs_to :area
   belongs_to :show
