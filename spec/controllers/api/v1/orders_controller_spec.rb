b6859e6065a47a84eb9a79ca3ac6eb52ba65dfef
xofred <fwin3000@gmail.com>
2015-07-24 17:54:30 +0800

    bug fix: 测试因应出票流程变化更改

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -35,15 +35,17 @@ RSpec.describe Api::V1::OrdersController, :type => :controller do
   context "#show_for_qr_scan" do
     it "should return tickets" do
       @order = Order.first
-      3.times { create(:show_area_relation) }
-      @order.set_tickets_and_price(ShowAreaRelation.all)
-      @order.success_pay!
+      relation = create :show_area_relation
+      3.times.each do |variable|
+        create(:ticket, area_id: relation.area_id, show_id: relation.show_id, price: relation.price)
+      end
+      @order.create_tickets_by_relations(relation, 3)
       @admin = create(:admin, admin_type: 2)
 
       get :show_for_qr_scan, name: @admin.name, api_token: @admin.api_token, id: @order.out_id, format: :json
       expect(assigns(:order)).to eq @order
       expect(response.body).to include("tickets")
-      expect(JSON.parse(response.body)["tickets"].count > 0).to be true
+      expect(JSON.parse(response.body)["tickets"].count).to eq 3 
     end
 
     it "should fail if admin is not ticket-checker" do
