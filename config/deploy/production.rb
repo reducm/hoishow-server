62b1ce4b3530d0e40ab17ef42cdf6924bfb3fceb
Jed_Huo <jed@bestapp.us>
2015-07-28 10:50:15 +0800

    change deploy file with production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,12 +8,12 @@ set :deploy_via, :remote_cache
 set :repo_url, "git@git.bestapp.us:root/hoishow-server.git"
 
 set :scm, :git
-set :branch, "staging"
+set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :use_sudo, false
 set :user, 'deploy'
 set :rails_env, "production"
-set :deploy_to, '/data/deploy/hoishow-server'
+set :deploy_to, '/data/deploy/hoishow-server-pt'
 
 server '10.6.31.207', user: 'deploy', roles: %w{web app db}
 server '10.6.23.41', user: 'deploy', roles: %w{web app db}
