ac3f48fc709aba0fc31da755bb187329a5f08b43
Jed_Huo <jed@bestapp.us>
2015-07-31 17:36:34 +0800

    local precompile task

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,86 @@ set :linked_files, %w{config/database.yml config/settings/wx_pay.yml config/sett
 # set the locations that we will look for changed assets to determine whether to precompile
 set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
+set :local_tmp_path, 'config/deploy/tmp/'
+
+set :keep_releases, 5
+
+def rsync(role, from, to)
+  options = [:rsync, '-av']
+  options << "-e 'ssh -p #{role.port}'" unless role.port.nil?
+  options << from
+  options << to
+  execute *options
+end
+
 namespace :deploy do
+  # desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     # Your restart mechanism here, for example:
+  #     execute :touch, current_path.join('tmp/restart.txt')
+  #   end
+  # end
+
+  namespace :assets do
+
+    def staged_assets_root_path
+      Pathname.new(fetch(:staged_assets_path) || 'config/deploy/assets/')
+    end
+
+    Rake::Task["precompile"].clear_actions
+    desc "Precompile assets locally and then rsync to deploy server"
+    task :precompile do
+
+      invoke 'deploy:assets:update_local_files_via_git'
+      invoke 'deploy:assets:local_precompile'
+    end
+
+    task :update_local_files_via_git do
+      run_locally{ execute :git, :pull, :origin, '`git rev-parse --abbrev-ref HEAD`' }
+    end
+
+    task :local_precompile do
+
+      assets_prefix = fetch(:assets_prefix)
+
+      assets_path = Pathname.new("./public/#{assets_prefix}")
+      run_locally do
+        execute :rm, '-rf', assets_path
+        staged_asstes_path = staged_assets_root_path.join(fetch(:stage).to_s)
+        execute :mkdir, '-p', staged_asstes_path
+        execute :ln, '-s', staged_asstes_path.realpath, assets_path
+        with rails_env: 'development' do
+          rake 'assets:precompile'
+          rake 'assets:clean'
+        end
+      end
+
+      #remove all manifest incase there is more than 1 manifest file
+      invoke 'deploy:assets:remove_manifest'
+      invoke 'deploy:assets:upload_local_assets'
+
+    end
+
+    task :upload_local_assets do
+
+      assets_prefix = fetch(:assets_prefix)
+      run_locally do
+        roles(*fetch(:assets_roles)).each do |role|
+          p "release_path-----------#{release_path}\n"
+          rsync role, "./public/#{assets_prefix}/",
+                  "#{role.user}@#{role.hostname}:#{release_path}/public/#{assets_prefix}/"
+        end
+      end
+    end
+
+    task :remove_manifest do
+      on roles *fetch(:assets_roles) do
+        execute :rm, '-f', release_path.join('public', 'assets', 'manifest*')
+      end
+    end
+  end
+
   task :compile_assets do
     on roles(:app) do
       within release_path do
