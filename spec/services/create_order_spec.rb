6d781e220ac6ac3c5510d59ee1a250dcf8c3d698
xofred <fwin3000@gmail.com>
2015-08-31 12:10:47 +0800

    order channel是hoishow或者bike_ticket

--- spec/services/create_order_spec.rb
+++ spec/services/create_order_spec.rb
@@ -86,7 +86,13 @@ describe CreateOrderLogic do
 
     it 'will handle pending_orders' do
       pending_order = user.orders.init_from_show(@show1)
-      pending_order.channel = 'ios'
+      #enum channel: {
+        #hoishow: 0, # Hoishow
+        #bike_ticket: 1 # 单车电影
+      #}
+      # fix ./spec/services/create_order_spec.rb:87
+      #   'ios' is not a valid channel
+      pending_order.channel = 'hoishow' 
       pending_order.save
       #pending_order.create_tickets_by_relations(@show1.show_area_relations.first, 1)
       expect(pending_order.status).to eq 'pending'
