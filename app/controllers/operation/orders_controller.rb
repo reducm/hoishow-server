dfa8c9547d7e989d7ce0ff282e5fb38761086b35
karra <karra@bestapp.us>
2015-09-02 17:47:44 +0800

    fix sms to worker

--- app/controllers/operation/orders_controller.rb
+++ app/controllers/operation/orders_controller.rb
@@ -75,7 +75,7 @@ class Operation::OrdersController < Operation::ApplicationController
   end
 
   def manual_send_sms
-    ChinaSMS.to(@order.user.mobile, "您订购的演出门票已发货，顺丰速运：#{@order.express_id}。可使用客户端查看订单及物流信息。客服电话：4008805380【单车娱乐】")
+    SendSmsWorker.perform_async(@order.user_mobile, "您订购的演出门票已发货，顺丰速运：#{@order.express_id}。可使用客户端查看订单及物流信息。客服电话：4008805380【单车娱乐】")
 
     flash[:notice] = '短信发送成功'
     redirect_to operation_orders_url
