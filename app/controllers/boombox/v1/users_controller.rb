df38f3f90a171b228b3e7db6a0b91cd6138da8b3
tom <tom@bestapp.us>
2015-10-15 16:17:26 +0800

    创建user_controller和更新route以及为controller添加action

---
+++ app/controllers/boombox/v1/users_controller.rb
@@ -0,0 +1,87 @@
+class Boombox::V1::UsersController < Boombox::V1::ApplicationController
+  before_filter :check_login!, except: [:verification, :verified_mobile, :sign_up, :sign_in]
+
+  def verification
+
+  end
+
+  def verified_mobile
+
+  end
+
+  def sign_up
+
+  end
+
+  def sign_in
+
+  end
+
+  def forgot_password
+
+  end
+
+  def reset_password
+
+  end
+
+  def update_user
+
+  end
+
+  def reset_mobile
+
+  end
+
+  def get_user
+
+  end
+
+  def followed_collaborators
+
+  end
+
+  def followed_playlists
+
+  end
+
+  def my_playlists
+
+  end
+
+  def comment_list
+
+  end
+
+  def message_list
+
+  end
+
+  def follow_subject
+
+  end
+
+  def unfollow_subject
+
+  end
+
+  def create_comment
+
+  end
+   
+  def like_subject
+
+  end
+
+  def unlike_subject
+
+  end
+
+  def add_to_playlist
+
+  end
+
+  def create_playlist
+
+  end
+end
--- config/routes/boombox.rb
+++ config/routes/boombox.rb
@@ -9,6 +9,31 @@ module Routes
               # api about fetch and query info
               resources :banners, only: :index
               resources :collaborators, only: [:index, :show]
+              resources :users, only: [] do
+                collection do
+                  get "verification"
+                  get "verified_mobile"
+                  post "sign_up"
+                  post "sign_in"
+                  post "forgot_password"
+                  post "reset_password"
+                  post "update_user"
+                  post "reset_mobile"
+                  get "get_user"
+                  get "followed_collaborators"
+                  get "followed_playlists"
+                  get "my_playlists"
+                  get "comment_list"
+                  get "message_list"
+                  post "follow_subject"
+                  post "unfollow_subject"
+                  post "create_comment"
+                  post "like_subject"
+                  post "unlike_subject"
+                  post "add_to_playlist"
+                  post "create_playlist"
+                end
+              end
             end
           end
         end
