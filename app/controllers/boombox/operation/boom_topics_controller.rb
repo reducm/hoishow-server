3ba9f7bf04551f769dd43ff2d0311758018fe140
xofred <fwin3000@gmail.com>
2015-11-09 17:59:25 +0800

    动态列表

--- app/controllers/boombox/operation/boom_topics_controller.rb
+++ app/controllers/boombox/operation/boom_topics_controller.rb
@@ -3,11 +3,47 @@ class Boombox::Operation::BoomTopicsController < Boombox::Operation::Application
   load_and_authorize_resource
 
   def index
+    params[:page] ||= 1
+    params[:per] ||= 10
+    # 按关键词过滤
+    if params[:q].present?
+      @boom_topics = @boom_topics.where("content like :search", search: "%#{params[:q]}%")
+    end
+    # 分页，每页显示数量
+    @boom_topics = @boom_topics.page(params[:page]).per(params[:per])
+    # 将参数回传 
+    @is_top = params[:is_top]
+    @per = params[:per]
   end
 
   # 置顶 
   def set_top 
+    collaborator = @boom_topic.collaborator
     @boom_topic.update_attributes(is_top: true)
-    redirect_to boombox_operation_boom_topics_url, notice: '置顶成功'
+    redirect_to boombox_operation_collaborator_url(collaborator), notice: '置顶成功'
+  end
+
+  def show
+    # comments/users
+    params[:page] ||= 1
+    params[:per] ||= 10
+
+    comments = @boom_topic.boom_comments 
+    if params[:q]
+      comments = comments.where("content like :search", search: "%#{params[:q]}%")
+    end
+    @comments = comments.page(params[:page]).per(params[:per])
+
+    @users = @boom_topic.likers.page(params[:page]).per(params[:per])
+  end
+
+  def destroy 
+    collaborator = @boom_topic.collaborator
+    if @boom_topic.delete
+      redirect_to boombox_operation_collaborator_url(collaborator), notice: '删除成功'
+    else
+      flash[:alert] = @boom_topic.errors.full_messages.to_sentence
+      render action: 'show'
+    end
   end
 end
