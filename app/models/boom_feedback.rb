a8c0fdcbdbd05e9f0133ebe7b8b97caa7f73b63c
tom <tom@bestapp.us>
2015-11-09 11:41:46 +0800

    add model:boom_feedbacks

---
+++ app/models/boom_feedback.rb
@@ -0,0 +1,5 @@
+class BoomFeedback < ActiveRecord::Base
+  acts_as_cached(:version => 1, :expires_in => 1.week)
+  default_scope {order('boom_feedbacks.created_at DESC')}
+  paginates_per 10
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023074359) do
+ActiveRecord::Schema.define(version: 20151109033719) do
 
   create_table "activity_statuses", force: :cascade do |t|
     t.string   "boom_id",    limit: 255
@@ -201,6 +201,13 @@ ActiveRecord::Schema.define(version: 20151023074359) do
     t.text     "content",       limit: 65535
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "boom_feedbacks", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.string   "contact",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "boom_locations", force: :cascade do |t|
