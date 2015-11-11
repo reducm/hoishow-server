5da92ec19fe8438bedae9ae2ba431588163ba357
xofred <fwin3000@gmail.com>
2015-11-11 17:21:11 +0800

    优化艺人列表页的js代码，详情页显示条目数
    艺人
    - 列表页过滤ajax在coffee里绑定，js erb不重复绑
    - 详情页tab加上条目数
    
    动态
    - 文本过滤等合并代码后，用es做

--- app/assets/javascripts/boombox/operation/collaborators.coffee
+++ app/assets/javascripts/boombox/operation/collaborators.coffee
@@ -1,8 +1,3 @@
 $ ->
-  $('select#per').change ->
-    $('#collaborators_form').submit() if $('#collaborators_form').length > 0
-    $('#topics_form').submit() if $('#topics_form').length > 0
-
-  $('input[type=submit]').change ->
-    $('#collaborators_form').submit() if $('#collaborators_form').length > 0
-    $('#topics_form').submit() if $('#topics_form').length > 0
+  $(document).on 'change', '.collaborators_filter', ->
+    $('#collaborators_form').submit()
--- app/controllers/boombox/operation/collaborators_controller.rb
+++ app/controllers/boombox/operation/collaborators_controller.rb
@@ -14,8 +14,6 @@ class Boombox::Operation::CollaboratorsController < Boombox::Operation::Applicat
     if params[:q].present?
       collaborators = collaborators.where("collaborators.name like :search", search: "%#{params[:q]}%")
     end
-    # 艺人数
-    @collaborators_count = collaborators.count
     # 分页，每页显示数量
     @collaborators = collaborators.order(created_at: :desc).page(params[:page]).per(params[:per])
 
@@ -27,15 +25,9 @@ class Boombox::Operation::CollaboratorsController < Boombox::Operation::Applicat
 
   def show
     # timeline
-    params[:page] ||= 1
-    params[:per] ||= 10
-    boom_topics = @collaborator.boom_topics
-    ## 按关键词过滤
-    #if params[:q].present?
-      #boom_topics = boom_topics.where("content like :search", search: "%#{params[:q]}%")
-    #end
-    # 分页，每页显示数量
-    @boom_topics = boom_topics.order(created_at: :desc).page(params[:page]).per(params[:per])
+    params[:topics_page] ||= 1
+    params[:topics_per] ||= 10
+    @boom_topics = @collaborator.boom_topics.order(created_at: :desc).page(params[:topics_page]).per(params[:topics_per])
 
     # tracks/playlists/shows/fans
     @boom_tracks = @collaborator.boom_tracks.order(created_at: :desc).page(1).per(10)
