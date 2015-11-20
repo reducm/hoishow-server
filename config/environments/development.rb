3fa2e229dcbb9afdfb885d021b73e8beb3c362fc
xofred <fwin3000@gmail.com>
2015-11-20 17:09:44 +0800

    昵称修改限制先不做；还原环境发邮件配置

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.cache_store = [:dalli_store, '127.0.0.1', {:namespace => "hsd", :compress => true}]
 
-  #config.action_mailer.default_url_options = { :host => 'dan-che.com' }
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'dan-che.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.exmail.qq.com",
