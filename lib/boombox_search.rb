c6cb741bfb7ee6420286559a0f1e84cec6070a27
karra <karra@bestapp.us>
2015-11-25 16:56:12 +0800

    fix search

--- lib/boombox_search.rb
+++ lib/boombox_search.rb
@@ -2,10 +2,10 @@ module BoomboxSearch
   extend self
 
   def query_search(key)
-    collaborators = Collaborator.verified.search(key).records.to_a
-    activities = BoomActivity.is_display.search(key).records.to_a
-    playlists = BoomPlaylist.playlist.open.search(key).records.to_a
-    tracks = BoomTrack.valid.search(key).records.to_a
+    collaborators = Collaborator.search(key).records.verified.to_a
+    activities = BoomActivity.search(key).records.is_display.to_a
+    playlists = BoomPlaylist.search(key).records.playlist.open.to_a
+    tracks = BoomTrack.search(key).records.valid.to_a
 
     {
       collaborators: collaborators,
