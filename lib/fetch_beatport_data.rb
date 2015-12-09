b84c754ce755a41222a2d2a69335c1212fd6c2c5
tom <tom@bestapp.us>
2015-12-09 16:49:54 +0800

    删除多余代码

--- lib/fetch_beatport_data.rb
+++ lib/fetch_beatport_data.rb
@@ -2,12 +2,4 @@ require 'fetch_beatport_data/beatport_logger'
 require 'fetch_beatport_data/fetch_bp_data'
 
 module FetchBeatportData
-  def self.read_data
-    File.open("public/temp.json", "r") do |file|
-      file.each do |line|
-        l = JSON.parse(line)
-        p l
-      end
-    end
-  end
 end
