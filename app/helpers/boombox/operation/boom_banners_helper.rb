e6f0e6e1b7bae21da24be03abeb5dbabca1f8c44
tom <tom@bestapp.us>
2015-12-14 18:25:38 +0800

    fix preventDefault

--- app/helpers/boombox/operation/boom_banners_helper.rb
+++ app/helpers/boombox/operation/boom_banners_helper.rb
@@ -1,2 +1,12 @@
 module Boombox::Operation::BoomBannersHelper
+  def subject_type_option(subject_type)
+    case subject_type
+    when "Collaborator"
+      [["艺人", "Collaborator"], ["活动", "Activity"], ["playlist", "Playlist"]]
+    when "BoomPlaylist"
+      [["playlist", "Playlist"], ["艺人", "Collaborator"], ["活动", "Activity"]]
+    when "BoomActivity"
+      [["活动", "Activity"], ["艺人", "Collaborator"], ["playlist", "Playlist"]]
+    end
+  end
 end
