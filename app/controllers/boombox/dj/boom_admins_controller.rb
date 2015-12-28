65921985d4effd3fb69e71abc0d62a72eff3af0a
xofred <fwin3000@gmail.com>
2015-12-28 16:08:33 +0800

    忘记密码功能
    - 用户填入邮箱后，会给该用户设置confirm_token（复用邮箱注册那个）
    - 用户点击邮件链接跳转到修改密码页面（url带上该用户的confirm_token）

--- app/controllers/boombox/dj/boom_admins_controller.rb
+++ app/controllers/boombox/dj/boom_admins_controller.rb
@@ -51,6 +51,54 @@ class Boombox::Dj::BoomAdminsController < Boombox::Dj::ApplicationController
     end
   end
 
+  def send_reset_password_email
+    if params[:boom_admins].present?
+      if @boom_admin = BoomAdmin.dj.where(email: params[:boom_admins][:email]).first
+        @boom_admin.set_confirmation_token
+        BoomAdminMailer.forget_password(@boom_admin).deliver_now
+        flash[:notice] = "发送成功！请登录您的邮箱按照提示操作"
+        redirect_to boombox_dj_after_send_reset_password_email_url(boom_admin_id: @boom_admin.id)
+      else
+        flash[:alert] = "邮箱未注册"
+        redirect_to boombox_dj_signin_url
+      end
+    # 重新发送
+    elsif params[:boom_admin_id].present?
+      if @boom_admin = BoomAdmin.dj.where(id: params[:boom_admin_id]).first
+        @boom_admin.set_confirmation_token
+        BoomAdminMailer.forget_password(@boom_admin).deliver_now
+        flash[:notice] = "发送成功！请登录您的邮箱按照提示操作"
+        redirect_to boombox_dj_after_send_reset_password_email_url(boom_admin_id: @boom_admin.id)
+      else
+        flash[:alert] = "未授权操作"
+        redirect_to boombox_dj_signin_url
+      end
+    end
+  end
+
+  def new_password
+    unless params[:token].present? && BoomAdmin.dj.where(confirm_token: params[:token]).any?
+      flash[:alert] = "未授权操作"
+      redirect_to boombox_dj_signin_url
+    end
+  end
+
+  def reset_password
+    @boom_admin = BoomAdmin.find(params[:id])
+    if params[:password].present? && params[:password_confirmation].present? && params[:password] == params[:password_confirmation]
+      if @boom_admin.reset_password!(params[:password])
+        flash[:notice] = "密码重置成功，请使用新密码登录"
+        redirect_to boombox_dj_signin_url
+      else
+        flash[:alert] = @boom_admin.errors.full_messages.to_sentence
+        redirect_to new_password_boombox_dj_boom_admin_url(@boom_admin, token: @boom_admin.confirm_token)
+      end
+    else
+      flash[:alert] = "密码为空或密码不一致，请重新输入"
+      redirect_to new_password_boombox_dj_boom_admin_url(@boom_admin, token: @boom_admin.confirm_token)
+    end
+  end
+
   private
   def boom_admin_params
     params.require(:boom_admin).permit(:name, :email)
