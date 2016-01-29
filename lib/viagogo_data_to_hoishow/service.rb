81c1ebc6682a582e91bcf887d10444869b637cae
tom <tom@bestapp.us>
2016-01-29 20:20:48 +0800

    fix bug

--- lib/viagogo_data_to_hoishow/service.rb
+++ lib/viagogo_data_to_hoishow/service.rb
@@ -14,95 +14,99 @@ module ViagogoDataToHoishow
 
         event_path_array = client.hgetall("viagogo_hot_events", :list).map{|x| JSON.parse x}.flatten
 
-        Star.transaction do
-          event_path_array.each do |event_path|
+        begin
+          Star.transaction do
+            event_path_array.each do |event_path|
 
-            temp_data = client.hget("viagogo_events_info", event_path)
-            if temp_data
-              event_info_array = JSON.parse temp_data
-            else
-              next
-            end
-
-            if event_info_array.present?
-              star_name = event_path.split("/").last[0..-9]
-              star = Star.where(name: star_name, event_path: event_path).first
-              if star
-                concert = Concert.where(name: "#{star.id}(自动生成)").first
-                return nil unless concert
-              else
-                star = Star.create(name: star_name, event_path: event_path)
-                concert = Concert.create(name: "#{star.id}(自动生成)", is_show: "auto_hide", status: "finished")
-              end
-              star.hoi_concert(concert)
-            else
-              next
-            end
-
-            #格式为{"stadium_name" => [{}...],...}
-            #一个stadium为一个show,不同日期为不同场次
-            event_info_group_by_stadium_hash = event_info_array.group_by{|x| x["VenueName"]}
-            event_info_group_by_stadium_hash.each do |info_hash|
-              stadium_name = info_hash[0]
-              temp_data = info_hash[1].select{|x| x["VenueCity"].present?}.first
+              temp_data = client.hget("viagogo_events_info", event_path)
               if temp_data
-                city_name = temp_data["VenueCity"]
+                event_info_array = JSON.parse temp_data
               else
                 next
               end
-              city = City.where(name: city_name).first_or_create
-              stadium = Stadium.where(name: stadium_name, city_id: city.id).first_or_create
-              show = Show.where(name: (star_name + " In " + stadium_name), concert_id: concert.id, city_id: city.id, stadium_id: stadium.id).first_or_create(source: 4, ticket_type: 1, mode: 1, status: 0, seat_type: 1)
-              info_hash[1].each do |single_event_info_hash|
-                show_time = single_event_info_hash["DateVal"]
 
-                ticket_info_path = single_event_info_hash["EventUrl"]
+              if event_info_array.present?
+                star_name = event_path.split("/").last[0..-9]
+                star = Star.where(name: star_name, event_path: event_path).first
+                if star
+                  concert = Concert.where(name: "#{star.id}(自动生成)").first
+                  return nil unless concert
+                else
+                  star = Star.create(name: star_name, event_path: event_path)
+                  concert = Concert.create(name: "#{star.id}(自动生成)", is_show: "auto_hide", status: "finished")
+                end
+                star.hoi_concert(concert)
+              else
+                next
+              end
 
-                temp_data = client.hget("viagogo_tickets_info", ticket_info_path)
+              #格式为{"stadium_name" => [{}...],...}
+              #一个stadium为一个show,不同日期为不同场次
+              event_info_group_by_stadium_hash = event_info_array.group_by{|x| x["VenueName"]}
+              event_info_group_by_stadium_hash.each do |info_hash|
+                stadium_name = info_hash[0]
+                temp_data = info_hash[1].select{|x| x["VenueCity"].present?}.first
                 if temp_data
-                  ticket_info_array = JSON.parse temp_data
+                  city_name = temp_data["VenueCity"]
                 else
                   next
                 end
+                city = City.where(name: city_name).first_or_create
+                stadium = Stadium.where(name: stadium_name, city_id: city.id).first_or_create
+                show = Show.where(name: (star_name + " In " + stadium_name), concert_id: concert.id, city_id: city.id, stadium_id: stadium.id).first_or_create(source: 4, ticket_type: 1, mode: 1, status: 0, seat_type: 1)
+                info_hash[1].each do |single_event_info_hash|
+                  show_time = single_event_info_hash["DateVal"]
 
-                if ticket_info_array.present?
-                  if event = Event.where(ticket_path: ticket_info_path).first
-                    event.update(show_time: show_time, show_id: show.id)
+                  ticket_info_path = single_event_info_hash["EventUrl"]
+
+                  temp_data = client.hget("viagogo_tickets_info", ticket_info_path)
+                  if temp_data
+                    ticket_info_array = JSON.parse temp_data
                   else
-                    event = Event.create(ticket_path: ticket_info_path, show_time: show_time, show_id: show.id)
+                    next
                   end
-                else
-                  next
-                end
 
-                #格式为{"section_id" => [{},...],...}
-                ticket_info_group_by_section_hash = ticket_info_array.group_by{|x| x["Section"]}
-                ticket_info_group_by_section_hash.each do |name_info_hash|
-                  ticket_info_group_by_section_array = name_info_hash[1]
-                  area_name = name_info_hash[0]
-
-                  next if area_name.blank?
-
-                  seats_count = ticket_info_group_by_section_array.inject(0){|sum, hash| sum + hash["MaxQuantity"]}
-                  price_array = ticket_info_group_by_section_array.map{|x|x["RawPrice"]}.sort
-                  max_price = price_array.last
-                  price_range = "#{price_array.first} - #{max_price}"
-
-                  if area = Area.where(name: area_name, event_id: event.id, stadium_id: stadium.id).first
-                    area.update(seats_count: seats_count, left_seats: seats_count)
-                    if relation = ShowAreaRelation.where(show_id: show.id, area_id: area.id).first
-                      relation.update(price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                  if ticket_info_array.present?
+                    if event = Event.where(ticket_path: ticket_info_path).first
+                      event.update(show_time: show_time, show_id: show.id)
                     else
-                      ShowAreaRelation.create(show_id: show.id, area_id: area.id, price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                      event = Event.create(ticket_path: ticket_info_path, show_time: show_time, show_id: show.id)
                     end
                   else
-                    area = Area.create(name: area_name, event_id: event.id, stadium_id: stadium.id, seats_count: seats_count, left_seats: seats_count)
-                    ShowAreaRelation.create(show_id: show.id, area_id: area.id, price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                    next
+                  end
+
+                  #格式为{"section_id" => [{},...],...}
+                  ticket_info_group_by_section_hash = ticket_info_array.group_by{|x| x["Section"]}
+                  ticket_info_group_by_section_hash.each do |name_info_hash|
+                    ticket_info_group_by_section_array = name_info_hash[1]
+                    area_name = name_info_hash[0]
+
+                    next if area_name.blank?
+
+                    seats_count = ticket_info_group_by_section_array.inject(0){|sum, hash| sum + hash["MaxQuantity"]}
+                    price_array = ticket_info_group_by_section_array.map{|x|x["RawPrice"]}.sort
+                    max_price = price_array.last
+                    price_range = "#{price_array.first} - #{max_price}"
+
+                    if area = Area.where(name: area_name, event_id: event.id, stadium_id: stadium.id).first
+                      area.update(seats_count: seats_count, left_seats: seats_count)
+                      if relation = ShowAreaRelation.where(show_id: show.id, area_id: area.id).first
+                        relation.update(price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                      else
+                        ShowAreaRelation.create(show_id: show.id, area_id: area.id, price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                      end
+                    else
+                      area = Area.create(name: area_name, event_id: event.id, stadium_id: stadium.id, seats_count: seats_count, left_seats: seats_count)
+                      ShowAreaRelation.create(show_id: show.id, area_id: area.id, price: max_price, price_range: price_range, seats_count: seats_count, left_seats: seats_count, third_inventory: seats_count)
+                    end
                   end
                 end
               end
             end
           end
+        rescue Exception => e
+          viagogo_logger.info "exception: #{e}"
         end
       end
     end
@@ -110,9 +114,8 @@ module ViagogoDataToHoishow
     def update_star_avatar
       client = SSDB::Client.new.connect
       if client.connected?
-        #Star.all.each do |star|
         #只更新avatar为nil的star
-        Star.where(avatar: nil).each do |star|
+        Star.where("event_path is not null and avatar is null").each do |star|
           event_path = star.event_path
           if event_path
             cover_url = client.hget("viagogo_event_covers", event_path)
@@ -139,9 +142,8 @@ module ViagogoDataToHoishow
     def update_event_stadium_map
       client = SSDB::Client.new.connect
       if client.connected?
-        #Event.all.each do |event|
         #只更新stadium_map为nil的event
-        Event.where(stadium_map: nil).each do |event|
+        Event.where("ticket_path is not null and stadium_map is null").each do |event|
           ticket_path = event.ticket_path
           if ticket_path
             map_url = client.hget("viagogo_stadium_maps", ticket_path)
@@ -203,7 +205,7 @@ module ViagogoDataToHoishow
               next
             end
           rescue Exception => e
-            viagogo_logger.info "#{subject_class}_id: #{subject.id} exception, 即将重试"
+            viagogo_logger.info "#{subject_class}_id: #{subject.id} exception: #{e}, 即将重试"
             next
           end
         end
