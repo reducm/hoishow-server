edd4b1d683f93f7c26c1ce1455ed64ed0fab745d
tom <tom@bestapp.us>
2016-03-11 16:49:48 +0800

    fix bug 4

--- app/mailers/viagogo_mailer.rb
+++ app/mailers/viagogo_mailer.rb
@@ -2,10 +2,9 @@ class ViagogoMailer < ApplicationMailer
   default :from => "dc-notify@bestapp.us"
 
   def notify_user_ticket_pic(order)
-    @user = order.user
-    @show = order.show
+    @order = order
     #attachments['门票.pdf'] = File.read(order.ticket_pic.current_path)  
-    attachments['门票.pdf'] = File.read(order.ticket_pic_url)  
+    #attachments['门票.pdf'] = File.read(order.ticket_pic_url)  
     mail(to: "tom@bestapp.us", subject: "演出门票test")
   end
 
