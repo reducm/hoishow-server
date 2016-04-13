8a4458bee9df3d028076bf9e2e008fc35b494299
xofred <fwin3000@gmail.com>
2016-04-13 15:57:45 +0800

    修复库存处理的逻辑和代码；测试实时爬取票区数据方法

--- spec/factories/areas.rb
+++ spec/factories/areas.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
   factory :area do
     sequence(:name) {|n| "Fuck#{n}"}
     seats_count {Random.rand(10..100)}
+    left_seats {Random.rand(10..100)}
     association :stadium
+    source_id {Random.rand(10..100)}
+    factory :yongle_area do
+      source Area.sources['yongle']
+    end
   end
 end
