98b0120a5b75a54abc9d3dbc2d3fa2c1f01c7180
tom <tom@bestapp.us>
2016-05-13 15:54:40 +0800

    fix viagogo data

--- app/workers/update_viagogo_stadium_map_worker.rb
+++ app/workers/update_viagogo_stadium_map_worker.rb
@@ -1,6 +1,6 @@
 class UpdateViagogoStadiumMapWorker
   include Sidekiq::Worker
-  sidekiq_options queue: 'update_viagogo_stadium_map', retry: 5
+  sidekiq_options queue: 'update_viagogo_stadium_map', retry: 3
 
   def perform(event_id)
     ViagogoDataToHoishow::Service.update_event_stadium_map_with_api(event_id)
